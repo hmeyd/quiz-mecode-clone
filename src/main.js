14d0b864bc662c591de6bf3470f6241a9b550606
hmeyd <yyahmedsalm@example.com>
2025-03-31 02:08:42 +0200

    RECLAGE

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import './style.css'
-import {Questions} from "./question"
+import { Questions } from "./question";
 console.log(Questions);
 
 const app = document.querySelector("#app");
-
-
 const startButton = document.querySelector("#start");
-startButton.addEventListener("click", startQuiz)
+startButton.addEventListener("click", startQuiz);
 
 function startQuiz(event) {
   event.stopPropagation();
@@ -14,109 +12,110 @@ function startQuiz(event) {
   let score = 0;
   displayQuestion(currentQuestion);
 
-
   function clean() {
     while (app.firstElementChild) {
       app.firstElementChild.remove();
     }
-    const progress = getProgressBar(Questions.length, currentQuestion)
+    const progress = getProgressBar(Questions.length, currentQuestion);
     app.appendChild(progress);
   }
 
   function getProgressBar(max, value) {
     const progress = document.createElement("progress");
-    progress.setAttribute("max", max)
-    progress.setAttribute("value", value)
+    progress.setAttribute("max", max);
+    progress.setAttribute("value", value);
     return progress;
   }
+
   function displayQuestion(index) {
     clean();
     const question = Questions[index];
     
     if (!question) {
-
       displayFinishMessage();
       return;
     }
 
     const title = getTitleElement(question.question);
-    app.appendChild(title)
-    const answersDiv =createAnswers(question.answers);
+    app.appendChild(title);
+    const answersDiv = createAnswers(question.answers);
     app.appendChild(answersDiv);
 
-    const sumbitbutton = getSubmitbutton();
-    sumbitbutton.addEventListener("click", submit)
-    sumbitbutton.removeEventlistener
-    app.appendChild(sumbitbutton);
+    const submitButton = getSubmitButton();
+    submitButton.addEventListener("click", submit);
+    app.appendChild(submitButton);
   }
 
   function displayFinishMessage() {
     const h1 = document.createElement("h1");
     h1.innerText = "Bravo ! Tu as terminé le quiz.";
     const p = document.createElement("p");
-    p.innerText = `Tu as eu ${score} sur ${Questions.length} point!`
+    p.innerText = `Tu as eu ${score} sur ${Questions.length} points!`;
     app.appendChild(h1);
     app.appendChild(p);
   }
 
-  function submit(){
-    const selecteAnsewer = app.querrySelector('input[name="answer"]:checked')
-    const value = selecteAnsewer.value;
-    const question = Questions[currentQuestion]
+  function submit() {
+    const selectedAnswer = app.querySelector('input[name="answer"]:checked');
+    if (!selectedAnswer) return;
+    disableAllAnswers();
+    const value = selectedAnswer.value;
+    const question = Questions[currentQuestion];
     const isCorrect = question.correct === value;
     if (isCorrect) {
       score++;
     }
-    showFeedback(isCorrect, question.correct, value);
-    const feedback = getFeedbackMessage (isCorrect, question.correct)
-    app.appendChild(feedback);
-
-    displayNextQuestionButton();
-   }
+    showFeedback(isCorrect, question.correct);
+    displayNextQuestionButton(() => {
+      currentQuestion++;
+      displayQuestion(currentQuestion);
+    });
   }
 
-  function displayNextQuestionButton() {
-    let remainingTimeout = TIMEOUT;
+  function displayNextQuestionButton(callback) {
+    let remainingTimeout = 5000;
+    app.querySelector("button")?.remove();
+    
+    const nextButton = document.createElement("button");
+    nextButton.innerText = `Next (${remainingTimeout / 1000})`;
+    app.appendChild(nextButton);
 
-      app.querrySelector("button").remove();
-      
-      const nextButton = document.createElement("button")
+    const interval = setInterval(() => {
+      remainingTimeout -= 1000;
       nextButton.innerText = `Next (${remainingTimeout / 1000})`;
+    }, 1000);
 
-      app.appendChild(nextButton);
+    const timeout = setTimeout(() => {
+      handleNextQuestion();
+    }, remainingTimeout);
 
-      const interval = setInterval(() => {
-        remainingTimeout-= 10000;
-        nextButton.innerText = `Next (${remainingTimeout / 1000}s)`;
-      }, 1000);
-
-
-      const handleNextQuestion = () => {
-        currentQuestion++;
-        clearInterval(interval);
-        clearTimeout(timeout);
-        displayQuestion(currentQuestion);
-      }
-
-      const timeout = setTimeout(() => {
-        handleNextQuestion()
-        },TIMEOUT);
-
-        nextButton.addEventListener("click", () =>{
-
-        })
+    function handleNextQuestion() {
+      clearInterval(interval);
+      clearTimeout(timeout);
+      callback();
     }
 
-  function createAnswers(answers){
-    const answersDiv = document.createElement("div");
+    nextButton.addEventListener("click", handleNextQuestion);
+  }
 
+  function disableAllAnswers() {
+    const radioInputs = document.querySelectorAll('input[type="radio"]');
+    for (const radio of radioInputs) {
+      radio.disabled = true;
+    }
+  }
+
+  function createAnswers(answers) {
+    const answersDiv = document.createElement("div");
     answersDiv.classList.add("answers");
+
     for (const answer of answers) {
-      const label = getTitleElement(answer);
+      const label = getAnswerElement(answer);
       answersDiv.appendChild(label);
     }
     return answersDiv;
   }
+}
 
 function getTitleElement(text) {
   const title = document.createElement("h3");
@@ -125,42 +124,33 @@ function getTitleElement(text) {
 }
 
 function formatId(text) {
-  const id = text.replaceAll(" ","-").tolowerCase();
+  return text.replaceAll(" ", "-").replaceAll('"', "'").toLowerCase();
 }
 
-function getAnswerElement(text){
+function getAnswerElement(text) {
   const label = document.createElement("label");
   label.innerText = text;
   const input = document.createElement("input");
   const id = formatId(text);
   input.id = id;
   label.htmlFor = id;
-  input.setAttribute ("type", "radio");
+  input.setAttribute("type", "radio");
   input.setAttribute("name", "answer");
   input.setAttribute("value", text);
   label.appendChild(input);
   return label;
 }
 
-function getSubmitbutton() {
-  const sumbitbutton = document.createElement("button");
-  sumbitbutton.innerText = "Submit";
-  return sumbitbutton;
+function getSubmitButton() {
+  const submitButton = document.createElement("button");
+  submitButton.innerText = "Submit";
+  return submitButton;
 }
-  const correctAnswerId = formatId(correct)
-  const correctElement = document.querySelector(
-    `label[for=${correctAnswerId}]`
-  )
 
-  const selectedAnswerId = formatId(answer)
-  const selectedElement = document.querySelector(
-    `label[for=${selectedAnswerId}]`
-  );
-
-  function getFeedbackMessage (isCorrect, correct) {
-    const paragraph =  document.createElement("p");
-    paragraph.innerText = isCorrect
-    ? `Bravo ! Tu as eu la bonne réponse`
-    : `Désolé.... mais la bonne réponse était ${correct}`;
-    return paragraph;
-  }
+function showFeedback(isCorrect, correct) {
+  const feedback = document.createElement("p");
+  feedback.innerText = isCorrect
+    ? "Bravo ! Tu as eu la bonne réponse."
+    : `Désolé... mais la bonne réponse était ${correct}.`;
+  app.appendChild(feedback);
+}
